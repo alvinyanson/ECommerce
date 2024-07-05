d6a987de6757d9fa2bf80eed590696036bb000d7
alvinyanson <a.yanson@arcanys.com>
2024-07-05 11:59:17 +0800

    feat: Get all products to display in Admin page

---
+++ ECommerceWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using ECommerce.DataAccess.Repository.IRepository;
+using ECommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceWeb.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> products = _unitOfWork.Product.GetAll();
+
+            return View(products);
+        }
+    }
+}
